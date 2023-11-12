3d32ecd61b13597147c3ff57a18cb190b427f2fb
Md. Alif Babu <muhammad.alifbabu@gmail.com>
2023-11-12 14:35:57 +0600

    initial commit

---
+++ src/main/java/com/example/beanmethod/BeanMethodApplication.java
@@ -0,0 +1,13 @@
+package com.example.beanmethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanMethodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beanmethod/BeanMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beanmethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
